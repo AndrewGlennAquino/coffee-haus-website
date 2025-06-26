9166e68ea4410c84695a330688a63984ce766586
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-26 15:27:59 -0500

    Add react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { motion } from "motion/react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
 
-function App() {
+const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
