ada610b55ca3155f1567c6f4ec8f34daca4ed33b
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-26 15:30:19 -0500

    Home page skeleton

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { motion } from "motion/react";
+
+const Home = () => {
+  return (
+    <>
+      <section id="home">
+        <div className="p-default">
+          <h1>Home</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
