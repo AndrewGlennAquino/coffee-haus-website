a47ca43809d2a0168c7f31c54e2872e917708c3f
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-26 17:44:30 -0500

    Add animation to sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,23 +22,95 @@ const Header = () => {
   };
 
   const middleVariant = {
-    animate: { opacity: 0 },
+    animateMiddle: { opacity: 0 },
+  };
+
+  // Animation variants for mobile links container
+  const sidebarVariants = {
+    initial: {
+      x: "75dvh",
+      transition: {
+        duration: 0.25,
+        ease: "easeIn",
+      },
+    },
+    animateSidebar: {
+      x: 0,
+      transition: {
+        duration: 0.25,
+        ease: "easeOut",
+      },
+    },
   };
 
   // Animation variants for monitor links
   const linkVariants = {
-    animate: { scale: 1.25 },
+    animateLinks: { scale: 1.25 },
   };
 
   return (
     <>
-      <header className="bg-babypowder shadow-lg sticky top-0 left-0 right-0">
+      <header
+        id="header"
+        className="bg-babypowder shadow-lg sticky top-0 left-0 right-0"
+      >
         <div className="font-cherry text-coffee mp-default flex justify-between items-center">
-          <h1 className="text-3xl lg:text-4xl flex items-center">
+          <h1 className="text-3xl lg:text-4xl flex justify-center items-center">
             Coffee Haus
           </h1>
 
-          {/* Monitor nav */}
+          {/* Hamburger nav */}
+          <nav className="xl:hidden">
+            <motion.button
+              className="w-12 h-12 flex flex-col justify-center gap-1.5"
+              animate={clicked ? "animate" : null}
+              onClick={handleClick}
+            >
+              <motion.div
+                className="bar"
+                animate={clicked ? "animateTop" : null}
+                variants={topBottomVariants}
+              />
+              <motion.div
+                className="bar"
+                animate={clicked ? "animateMiddle" : null}
+                variants={middleVariant}
+              />
+              <motion.div
+                className="bar"
+                animate={clicked ? "animateBottom" : null}
+                variants={topBottomVariants}
+              />
+            </motion.button>
+
+            {/* Hidden Links Container */}
+            <motion.div
+              className="bg-babypowder/25 w-1/2 sm:w-1/4 h-screen mp-default fixed top-20 right-0"
+              initial="initial"
+              animate={clicked ? "animateSidebar" : null}
+              variants={sidebarVariants}
+            >
+              <ul className="text-2xl flex flex-col items-end gap-12">
+                <motion.li whileTap="animateLinks" variants={linkVariants}>
+                  <Link to="/">Home</Link>
+                </motion.li>
+
+                <motion.li whileTap="animateLinks" variants={linkVariants}>
+                  <Link>Link 2</Link>
+                </motion.li>
+
+                <motion.li whileTap="animateLinks" variants={linkVariants}>
+                  <Link>Link 3</Link>
+                </motion.li>
+
+                <motion.li whileTap="animateLinks" variants={linkVariants}>
+                  <Link>Link 4</Link>
+                </motion.li>
+              </ul>
+            </motion.div>
+          </nav>
+
+          {/* Monitor Nav */}
           <nav className="hidden xl:flex flex-col justify-center">
             <ul className="text-2xl flex justify-between align-center gap-16">
               <motion.li whileHover="animate" variants={linkVariants}>
@@ -57,31 +129,6 @@ const Header = () => {
                 <Link>Link 4</Link>
               </motion.li>
             </ul>
-          </nav>
-
-          {/* Hamburger nav */}
-          <nav className="xl:hidden">
-            <motion.button
-              className="w-12 h-12 flex flex-col justify-center gap-1.5"
-              animate={clicked ? "animate" : null}
-              onClick={handleClick}
-            >
-              <motion.div
-                className="bar"
-                animate={clicked ? "animateTop" : null}
-                variants={topBottomVariants}
-              />
-              <motion.div
-                className="bar"
-                animate={clicked ? "animate" : null}
-                variants={middleVariant}
-              />
-              <motion.div
-                className="bar"
-                animate={clicked ? "animateBottom" : null}
-                variants={topBottomVariants}
-              />
-            </motion.button>
           </nav>
         </div>
       </header>
