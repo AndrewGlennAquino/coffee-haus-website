e1add797d53d538c005fae93ef0d52cf17b0f9bd
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-06-26 15:29:33 -0500

    Add functionality to hamburger menu and nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { motion } from "motion/react";
+import { Link } from "react-router-dom";
 
-function Header() {
+const Header = () => {
   const [clicked, setClicked] = useState(false); // Keep clicked in state to open and close hamburger menu
 
   /**
@@ -24,55 +25,68 @@ function Header() {
     animate: { opacity: 0 },
   };
 
-  const FakeComponent = () => {
-    return <div className="bg-black h-dvh"></div>;
+  // Animation variants for monitor links
+  const linkVariants = {
+    animate: { scale: 1.25 },
   };
-    const FakeComponent2 = () => {
-    return <div className="bg-babypowder h-dvh"></div>;
-  };
-
 
   return (
     <>
-      <header className="bg-babypowder font-cherry text-coffee flex justify-between align-center fixed top-0 left-0 right-0">
-        <h1 className="text-4xl md:text-5xl">Coffee Haus</h1>
+      <header className="bg-babypowder shadow-lg sticky top-0 left-0 right-0">
+        <div className="font-cherry text-coffee mp-default flex justify-between items-center">
+          <h1 className="text-3xl lg:text-4xl flex items-center">
+            Coffee Haus
+          </h1>
 
-        {/* Monitor nav */}
-        <nav className="hidden xl:flex flex-col justify-center">
-          <ul className="text-2xl flex justify-between align-center gap-16">
-            <li>Link 1</li>
+          {/* Monitor nav */}
+          <nav className="hidden xl:flex flex-col justify-center">
+            <ul className="text-2xl flex justify-between align-center gap-16">
+              <motion.li whileHover="animate" variants={linkVariants}>
+                <Link to="/">Home</Link>
+              </motion.li>
 
-            <li>Link 2</li>
+              <motion.li whileHover="animate" variants={linkVariants}>
+                <Link>Link 2</Link>
+              </motion.li>
 
-            <li>Link 3</li>
+              <motion.li whileHover="animate" variants={linkVariants}>
+                <Link>Link 3</Link>
+              </motion.li>
 
-            <li>Link 4</li>
-          </ul>
-        </nav>
+              <motion.li whileHover="animate" variants={linkVariants}>
+                <Link>Link 4</Link>
+              </motion.li>
+            </ul>
+          </nav>
 
-        {/* Hamburger nav */}
-        <nav className="xl:hidden">
-          <motion.button
-            className="w-12 h-12 flex flex-col justify-center gap-1.5"
-            animate={clicked ? "animate" : null}
-            onClick={handleClick}
-          >
-            <motion.div
-              className="bar"
-              animate={clicked ? "animateTop" : null}
-              variants={topBottomVariants}
-            />
-            <motion.div className="bar" variants={middleVariant} />
-            <motion.div
-              className="bar"
-              animate={clicked ? "animateBottom" : null}
-              variants={topBottomVariants}
-            />
-          </motion.button>
-        </nav>
+          {/* Hamburger nav */}
+          <nav className="xl:hidden">
+            <motion.button
+              className="w-12 h-12 flex flex-col justify-center gap-1.5"
+              animate={clicked ? "animate" : null}
+              onClick={handleClick}
+            >
+              <motion.div
+                className="bar"
+                animate={clicked ? "animateTop" : null}
+                variants={topBottomVariants}
+              />
+              <motion.div
+                className="bar"
+                animate={clicked ? "animate" : null}
+                variants={middleVariant}
+              />
+              <motion.div
+                className="bar"
+                animate={clicked ? "animateBottom" : null}
+                variants={topBottomVariants}
+              />
+            </motion.button>
+          </nav>
+        </div>
       </header>
     </>
   );
-}
+};
 
 export default Header;
